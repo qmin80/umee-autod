593a611ba5da26d1fb515b54e195e6310124cab6
nodebreaker0-0 <zeroday26@gmail.com>
2021-11-24 22:34:40 +0900

    tx_boom

--- main.go
+++ main.go
@@ -7,9 +7,7 @@ import (
 
 	"github.com/cosmos/cosmos-sdk/crypto/keys/secp256k1"
 	sdktypes "github.com/cosmos/cosmos-sdk/types"
-	"github.com/cosmos/cosmos-sdk/x/distribution/types"
-	distrtypes "github.com/cosmos/cosmos-sdk/x/distribution/types"
-	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
+	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
 	"github.com/rs/zerolog/log"
 
 	"github.com/nodebreaker0-0/umee-autod/client"
@@ -83,42 +81,18 @@ func (d *AccountDispenser) DecAccSeq() {
 	d.accSeq--
 }
 
-func MultiMsgWithdrawCommissionAndDelegate(valAddr string, delAddr string, coin sdktypes.Coin) (msgs []sdktypes.Msg, err error) {
-
-	withdrawMsg := &distrtypes.MsgWithdrawValidatorCommission{
-		ValidatorAddress: valAddr,
-	}
-
-	if err := withdrawMsg.ValidateBasic(); err != nil {
-		return []sdktypes.Msg{}, err
-	}
-	delegateMsg := &stakingtypes.MsgDelegate{
-		DelegatorAddress: delAddr,
-		ValidatorAddress: valAddr,
-		Amount:           coin,
-	}
-	if err := delegateMsg.ValidateBasic(); err != nil {
-		return []sdktypes.Msg{}, err
-	}
-
-	return []sdktypes.Msg{withdrawMsg, delegateMsg}, nil
-}
-
 func main() {
 	ctx := context.Background()
 
 	cfg, err := config.Read(config.DefaultConfigPath)
 	if err != nil {
 		println(err)
-
 	}
 	client, err := client.NewClient(cfg.RPC.Address, cfg.GRPC.Address)
 	if err != nil {
 		println(err)
 	}
 	defer client.Stop() // nolint: errcheck
-
-	queryClient := types.NewQueryClient(client.GRPC)
 
 	chainID, err := client.RPC.GetNetworkChainID(ctx)
 	if err != nil {
@@ -134,64 +108,22 @@ func main() {
 	if err := d.Next(); err != nil {
 		panic(fmt.Errorf("get next account: %w", err))
 	}
-	//st, err := client.RPC.Status(ctx)
-	//if err != nil {
-	//      panic(fmt.Errorf("get status: %w", err))
-	//}
-	//startingHeight := st.SyncInfo.LatestBlockHeight + 2
-	//log.Info().Msgf("current block height is %d, waiting for the next block to be committed", st.SyncInfo.LatestBlockHeight)
-
-	//if err := rpcclient.WaitForHeight(client.RPC, startingHeight-1, nil); err != nil {
-	//      panic(fmt.Errorf("wait for height: %w", err))
-	//}
-
-	//targetHeight := startingHeight
 
 	for {
-		//st, err := client.RPC.Status(ctx)
-		//if err != nil {
-		//      panic(fmt.Errorf("get status: %w", err))
-		//}
-		//if st.SyncInfo.LatestBlockHeight != targetHeight-1 {
-		//      log.Warn().Int64("expected", targetHeight-1).Int64("got", st.SyncInfo.LatestBlockHeight).Msg("mismatching block height")
-		//      targetHeight = st.SyncInfo.LatestBlockHeight + 1
-		//}
-		res, err := queryClient.ValidatorCommission(
-			ctx,
-			&types.QueryValidatorCommissionRequest{ValidatorAddress: cfg.Custom.ValidatorAddr},
-		)
-		if err != nil {
-			println(err)
-		}
-		delamount := res.GetCommission().Commission.String()
-		convertdelamount, err := sdktypes.ParseCoinNormalized(delamount)
-		if err != nil {
-			println(err)
+
+		coin := sdktypes.NewCoins(sdktypes.NewCoin("uumee", sdktypes.NewInt(1000)))
+		sendMsg := &banktypes.MsgSend{
+			FromAddress: "umee1fp0sc558kxrnc5rvyym4wesl365s2emkwkjvks",
+			ToAddress:   "umee1fp0sc558kxrnc5rvyym4wesl365s2emkwkjvks",
+			Amount:      coin,
 		}
 
-		accountcoins, err := client.GRPC.GetAllBalances(ctx, d.addr)
-		if err != nil {
-			println(err)
+		if err := sendMsg.ValidateBasic(); err != nil {
+			panic(fmt.Errorf("msgs err: %w", err))
 		}
 
-		println("delamount: ", convertdelamount.String())
-		if convertdelamount.String() == "0uumee" {
-			continue
-		}
-		println("accamount: ", accountcoins[0].String())
-		var msgs []sdktypes.Msg
-		totalamount := convertdelamount.Add(accountcoins[0])
-		msgs, err = MultiMsgWithdrawCommissionAndDelegate(cfg.Custom.ValidatorAddr, d.addr, totalamount.SubAmount(sdktypes.NewInt(450)))
-		if err != nil {
-			println(fmt.Errorf("msgs err: %w", err))
-			continue
-		}
-		fmt.Println(msgs)
-		if msgs == nil {
-			continue
-		}
 		accSeq := d.IncAccSeq()
-		txByte, err := tx.Sign(ctx, accSeq, d.AccNum(), d.PrivKey(), msgs...)
+		txByte, err := tx.Sign(ctx, accSeq, d.AccNum(), d.PrivKey(), sendMsg)
 		if err != nil {
 			panic(fmt.Errorf("sign tx: %w", err))
 		}
@@ -199,7 +131,7 @@ func main() {
 		if err != nil {
 			panic(fmt.Errorf("broadcast tx: %w", err))
 		}
-		println(resp.TxResponse.RawLog)
+
 		if resp.TxResponse.Code != 0 {
 			if resp.TxResponse.Code == 0x5 {
 				log.Warn().Msg("insufficient funds, stopping")
@@ -222,8 +154,6 @@ func main() {
 				panic(fmt.Sprintf("%#v\n", resp.TxResponse))
 			}
 		}
-
-		time.Sleep(13000 * time.Millisecond)
 
 	}
 
